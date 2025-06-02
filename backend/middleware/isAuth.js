8a3b2b0dcd9023dd2e02246df53394c3c491453c
raushan rawat <raushanvivekcyber@gmail.com>
2025-06-02 12:48:45 +0530

    Fix: removed submodule, added backend as regular folder

---
+++ backend/middleware/isAuth.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken"
+const isAuth = async (req , res ,next)=>{
+    try{
+        const token = req.cookies.token
+        if(!token){
+            return res.status(400).json({messsage:"token not found"})
+        }
+        const verifyToken = await jwt.verify(token,process.env.JWT_SECRET)
+        req.userId = verifyToken.userId;
+        next();
+    }catch(err){
+      console.log(err);
+      return res.status(500).json({messsage:"is Auth error"})
+    }
+}
+
+export default isAuth;
