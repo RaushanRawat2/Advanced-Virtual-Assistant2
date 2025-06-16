9af9b4496cdec654f46e5fabffbf742bf016da4a
Raushan Kumar <166208082+RaushanRawat2@users.noreply.github.com>
2025-06-16 19:38:09 +0530

    Update UserContext.jsx

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -3,7 +3,7 @@ import axios from "axios"
 export const userDataContext = createContext();
 
 function UserContext({children}) {
-    const serverUrl = "http://localhost:8000";
+    const serverUrl = "https://advanced-virtual-assistant-backend.onrender.com";
     const [userData,setUserData] = useState(null);
     const [frontendImage,SetFrontendImage]= useState(null);
     const [backendImage,setBackendImage]= useState(null);
